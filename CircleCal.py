346ddcebb26f834aff6621f838ea352e5720eb94
202010908 임현빈 <limhb708@naver.com>
2025-03-12 12:30:14 +0900

    202010908임현빈 2주차 과제제

--- CircleCal.py
+++ CircleCal.py
@@ -1,3 +1,3 @@
 one = float(input("원의 반지름을 입력하세요: "))
 # 변환 결과를 출력
-print(f"{one} 원의 둘레는 {one * math.pi*2} 넓이는{one * one * math.pi} 입니다.")
+print(f"{one} 원의 둘레는 {one * math.pi*2} 넓이는{one * one * math.pi} 입니다.")  
